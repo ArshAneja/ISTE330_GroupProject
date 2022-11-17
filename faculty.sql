6ffb93b33bc1f61c528962e1480d36468a964d87
ArshAneja <75496064+ArshAneja@users.noreply.github.com>
2022-11-16 19:24:42 -0500

    Update faculty.sql

--- faculty.sql
+++ faculty.sql
@@ -7,127 +7,127 @@ DROP DATABASE IF EXISTS academicSkills;
 CREATE DATABASE academicSkills;
 USE academicSkills;
 
-DROP TABLE IF EXISTS Faculty_Password;
+Drop table if exists Faculty_Password;
 DROP TABLE IF EXISTS Faculty_Skill;
-DROP TABLE IF EXISTS Abstract_Skill;
+drop table if exists Abstract_Skill;
 DROP TABLE IF EXISTS Skill;
 DROP TABLE IF EXISTS abstract;
-DROP TABLE IF EXISTS Faculty;
-DROP TABLE IF EXISTS Student_Skill;
-DROP TABLE IF EXISTS Student_Password;
-DROP TABLE IF EXISTS Student; 
+Drop table if exists Faculty;
+Drop table if exists Student_Skill;
+Drop table if exists Student_Password;
+Drop table if exists Student; 
 
 CREATE TABLE Faculty (
-    faculty_id INT AUTO_INCREMENT,
-    lastName VARCHAR(255),
-    firstName VARCHAR(255),
-    email VARCHAR(255),
-    officeNumber VARCHAR(255),
-    password VARCHAR(255),
-    PRIMARY KEY (faculty_id)
+    faculty_id int AUTO_INCREMENT,
+    lastName varchar(255),
+    firstName varchar(255),
+    email varchar(255),
+    officeNumber varchar(255),
+    password varchar(255),
+    primary key (faculty_id)
 );
 create table Skill(
-	skill_id INT NOT NULL auto_increment,
-    skill_name VARCHAR(255),
+	skill_id int not null auto_increment,
+    skill_name varchar(255),
 	key(skill_id)
 );
 
 Create table Faculty_Password(
-faculty_id INT NOT NULL,
-    encrypted_password VARCHAR(255) NOT NULL,
+faculty_id int NOT NULL,
+    encrypted_password varchar(255) NOT NULL,
     PRIMARY KEY (faculty_id),
     FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id)
 );
 
 create table Faculty_Skill(
-	faculty_id INT NOT NULL,
-    skill_id INT NOT NULL,
+	faculty_id int not null,
+    skill_id int not null,
 	FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id),
 	FOREIGN KEY (skill_id) REFERENCES Skill(skill_id)
 );
 
-Create table Abstract(
-	abstract_id INT NOT NULL,
-    faculty_id INT NOT NULL,
-    abstractName VARCHAR(255),
-    abstractSummary VARCHAR(255),
-    PRIMARY KEY (abstract_id),
+Create table abstract(
+	abstract_id int not null,
+    faculty_id int not null,
+    abstractName varchar(255),
+    abstractSummary varchar(255),
+     KEY (abstract_id),
     FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id)
 );
 
 create table Abstract_Skill(
-	abstract_id INT NOT NULL,
-    skill_id INT NOT NULL,
+	abstract_id int not null,
+    skill_id int not null,
 	FOREIGN KEY (abstract_id) REFERENCES abstract(abstract_id),
 	FOREIGN KEY (skill_id) REFERENCES Skill(skill_id)
 );
 
 create table Student(
-    student_id INT NOT NULL AUTO_INCREMENT,
-    lastName VARCHAR(255) NOT NULL,
-    firstName VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    grade VARCHAR(255),
-    password VARCHAR(255),
-    PRIMARY KEY (student_id)
+    student_id int not null AUTO_INCREMENT,
+    lastName varchar(255) not null,
+    firstName varchar(255) not null,
+    email varchar(255) not null,
+    grade varchar(255),
+    password varchar(255),
+    primary key (student_id)
 )ENGINE=InnoDB AUTO_INCREMENT=10000;
 
 Create table Student_Password(
-	student_id INT NOT NULL,
-    encrypted_password VARCHAR(255) NOT NULL,
+     student_id int NOT NULL,
+    encrypted_password varchar(255) NOT NULL,
     PRIMARY KEY (student_id),
     FOREIGN KEY (student_id) REFERENCES Student(student_id)
 );
 
 create table Student_Skill(
-	student_id INT NOT NULL,
-    skill_id INT NOT NULL AUTO_INCREMENT,
+	student_id int not null,
+    skill_id int not null auto_increment,
 	FOREIGN KEY (student_id) REFERENCES Student(student_id),
 	FOREIGN KEY (skill_id) REFERENCES Skill(skill_id)
 );
 
-INSERT Faculty VALUES(1,"John","David","dj@rit.edu","Gold","dj");
-INSERT Faculty VALUES(2,"Molly","Bonnie","mb@rit.edu","gol-234","password");
-INSERT Faculty_Password VALUES(1,'dj');
-INSERT Faculty_Password VALUES(2,'password');
 
 
-INSERT Skill VALUES(1,"php");
-INSERT Skill VALUES(1,"java");
-INSERT Skill VALUES(1,"python");
+insert Faculty values(1,"John","David","dj@rit.edu","Gold","dj");
+insert Faculty values(2,"Molly","Bonnie","mb@rit.edu","gol-234","password");
+insert Faculty_Password values(1,'dj');
+insert Faculty_Password values(2,'password');
 
-INSERT Skill VALUES(2,"html");
-INSERT Skill VALUES(2,"css");
-INSERT Skill VALUES(2,"ruby");
 
-INSERT faculty_skill VALUES(1,1);
-INSERT faculty_skill VALUES(2,2);
+insert Skill values(1,"php");
+insert Skill values(1,"java");
+insert Skill values(1,"python");
 
- INSERT abstract VALUES(1,1,"Python is cool","A summary of python being very cool.");
- INSERT abstract VALUES(2,2,"Java is super fun!","Java can be very complex, but what if I say theres any easier way to learn?");
+insert Skill values(2,"html");
+insert Skill values(2,"css");
+insert Skill values(2,"ruby");
+
+insert faculty_skill values(1,1);
+insert faculty_skill values(2,2);
+
+ insert abstract values(1,1,"Python is cool","A summary of python being very cool.");
+ insert abstract values(2,2,"Java is super fun!","Java can be very complex, but what if I say theres any easier way to learn?");
+  insert abstract values(2,2,"Java is super fun!","Java can be very complex, but what if I say theres any easier way to learn?");
+
  
- INSERT abstract_skill VALUES(1,1);
- INSERT abstract_skill values(2,2);
  
-INSERT Student(lastName,firstName, email) VALUES("Johnson","Jamal","jj@rit.edu");
-INSERT Student(lastName,firstName, email) VALUES("Morales","Miles","mm@rit.edu");
+ insert abstract_skill values(1,1);
+ insert abstract_skill values(2,2);
+ 
+insert Student(lastName,firstName, email, password) values("Johnson","Jamal","jj@rit.edu","ILovemyMom123");
+insert Student(lastName,firstName, email, password) values("Morales","Miles","mm@rit.edu","Spiderman12");
 
-INSERT Student_Password VALUES(10000,"ILovemyMom123");
-INSERT Student_Password VALUES(10001,"Spiderman12");
+insert Student_Password values(10000,"ILovemyMom123");
+insert Student_Password values(10001,"Spiderman12");
 
-INSERT Skill VALUES(10000,"php");
-INSERT Skill VALUES(10000,"java");
-INSERT Skill VALUES(10000,"python");
+insert Skill values(10000,"php");
+insert Skill values(10000,"java");
+insert Skill values(10000,"python");
 
-INSERT Skill VALUES(10001,"bash");
-INSERT Skill VALUES(10001,"css");
-INSERT Skill VALUES(10001,"C++");
 
-INSERT Student_Skill VALUES(10000,10000);
-INSERT Student_Skill VALUES(10000,10001);
+insert Skill values(10001,"bash");
+insert Skill values(10001,"css");
+insert Skill values(10001,"C++");
 
-SELECT * FROM Skill;
-
-SELECT Faculty.firstName, Skill.skill_name FROM Skill
-INNER JOIN Faculty_Skill ON Faculty_Skill.skill_id = Skill.skill_id
-INNER JOIN Faculty ON Faculty_Skill.faculty_id = Faculty.faculty_id;
+insert Student_Skill values(10000,10000);
+insert Student_Skill values(10000,10001);
